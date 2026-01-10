2b077bb53eeff00d5437dc22765fc54de9b68be4
Disha Andrapiya <dishaandrapiya1812@gmail.com>
2026-01-10 20:00:03 +0530

    Prepare backend for deployment

--- app.py
+++ app.py
@@ -390,4 +390,5 @@ def get_stats(current_user):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
